583b6228b080638f9eb32592d84d00f793c19140
Kuldeep kumar <117909055+kd-234@users.noreply.github.com>
2024-05-07 16:23:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function() {
+
+const panels = document.querySelectorAll('.panel');
+	
+	panels.forEach(panel => {
+		panel.addEventListener('click', function() {
+	// Remove 'active' class from all panels
+			panels.forEach(p => {
+				p.classList.remove('active');
+			});
+			// Add 'active' class to the clicked panel
+			this.classList.add('active');
+		});
+	});
+});
+
+// document.addEventListener('DOMContentLoaded', function() {
+ //        const panels = document.querySelectorAll('.panel');
+        
+ //        panels.forEach(panel => {
+ //            panel.addEventListener('click', function() {
+ //                // Remove 'active' class from all panels
+ //                panels.forEach(p => {
+ //                    p.classList.remove('active');
+ //                });
+                
+ //                // Add 'active' class to the clicked panel
+ //                this.classList.add('active');
+ //            });
+ //        });
+ //    });
